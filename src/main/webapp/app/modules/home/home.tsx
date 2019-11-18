367748cb54af2c42f23b1e26ab4df7eb7962356d
Vaibhaw Agrawal <vaibhaw.agrawal@publicissapient.com>
2019-11-18 16:25:09 +0530

    .

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -16,7 +16,7 @@ export const Home = (props: IHomeProp) => {
   return (
     <Row>
       <Col md="9">
-        <h2>Welcome, Java Hipster!</h2>
+        <h2>Welcome, Vaibhaw!</h2>
         <p className="lead">This is your homepage</p>
         {account && account.login ? (
           <div>
